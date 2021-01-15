904ae44c85d070d88f8c00db46ad4627c2157968
demon034 <sspahic2@etf.unsa.ba>
2021-01-15 19:56:18 +0100

    Import modula

--- spirala4/db.js
+++ spirala4/db.js
@@ -5,6 +5,13 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
+db.predmet = sequelize.import(__dirname+"/predmet.js");
+db.predmet = sequelize.import(__dirname+"/grupa.js");
+db.predmet = sequelize.import(__dirname+"/aktivnost.js");
+db.predmet = sequelize.import(__dirname+"/dan.js");
+db.predmet = sequelize.import(__dirname+"/tip.js");
+db.predmet = sequelize.import(__dirname+"/student.js");
+
 
 
 module.exports = db;
