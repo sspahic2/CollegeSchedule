5ab90fc25ce105636a7ab43532a156fced045749
demon034 <sspahic2@etf.unsa.ba>
2021-01-15 23:55:59 +0100

    Kreirana baza

--- spirala4/db.js
+++ spirala4/db.js
@@ -5,13 +5,24 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.predmet = sequelize.import(__dirname+"/predmet.js");
-db.predmet = sequelize.import(__dirname+"/grupa.js");
-db.predmet = sequelize.import(__dirname+"/aktivnost.js");
-db.predmet = sequelize.import(__dirname+"/dan.js");
-db.predmet = sequelize.import(__dirname+"/tip.js");
-db.predmet = sequelize.import(__dirname+"/student.js");
+db.predmet = require("./predmet.js")(sequelize, Sequelize.DataTypes);
+db.grupa = require("./grupa.js")(sequelize, Sequelize.DataTypes);
+db.aktivnost = require("./aktivnost.js")(sequelize, Sequelize.DataTypes);
+db.dan = require("./dan.js")(sequelize, Sequelize.DataTypes);
+db.tip = require("./tip.js")(sequelize, Sequelize.DataTypes);
+db.student = require("./student.js")(sequelize, Sequelize.DataTypes);
 
+db.predmet.hasMany(db.grupa, {as:"grupeNaPredmetu", foreignKey:{allowNull:false}});
 
+db.predmet.hasMany(db.aktivnost, {as:"aktivnostiIzPredmeta", foreignKey:{allowNull:false}});
+
+db.grupa.hasMany(db.aktivnost, {as:"aktivnostiGrupe", foreignKey:{allowNull:true}});
+
+db.dan.hasMany(db.aktivnost, {as:"aktivnostiUDanu", foreignKey:{allowNull:false}});
+
+db.tip.hasMany(db.aktivnost, {as:"aktivnostiTipa", foreignKey:{allowNull:false}});
+
+db.studentUGrupi = db.student.belongsToMany(db.grupa, {as:"grupe", through:"student_u_grupi", foreignKey:"grupaId"});
+db.grupa.belongsToMany(db.student, {as:"studenti", through:"student_u_grupi", foreignKey:"studentId"});
 
 module.exports = db;
