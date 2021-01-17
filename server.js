8e46a251157e4c1a5e5d2a0b7a971a6b4af942ec
demon034 <sspahic2@etf.unsa.ba>
2021-01-17 20:19:04 +0100

    Zavrsen prvi zadatak

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
-const db = require('./spirala4/db.js');
+const db = require('./spirala4/db.js')
+const { Op } = require('sequelize');
 
 //Prima se samo json input, svi ostali izbacuju error html
 app.use(bodyParser.json({
@@ -120,17 +121,17 @@ app.get('/v2/predmet/:naziv/grupe', function(req, res) {
 });
 
 app.get('/v2/grupe', function(req, res) {
-    db.grupa.findAll().then(function(elements) {
+    db.grupa.findAll({include:[{model:db.predmet}]}).then(function(elements) {
         let returnData = [];
         elements.forEach(element => {
-            returnData.push(element.dataValues);
+            returnData.push({id:element.dataValues.id, naziv:element.dataValues.naziv, predmet:element.dataValues.predmet});
         });
         res.json(returnData);
     });
 });
 
 app.get('/v2/grupa/:naziv', function(req, res) {
-    db.grupa.findOne({where:{naziv:req.params.naziv}}).then(function(data) {
+    db.grupa.findOne({where:{naziv:req.params.naziv}, include:[{model:db.predmet}]}).then(function(data) {
         if(data) {
             res.json(data.dataValues);
         }
@@ -153,21 +154,12 @@ app.get('/v2/grupa/:naziv/aktivnosti', function(req, res) {
 });
 
 app.get('/v2/grupe/studenti', function(req, res) {
-    db.grupa.findAll().then(function(elements) {
-        let grupe = [];
-        elements.forEach(grupa => {
-            let studenti = [];
-            grupa.getStudenti().then(function(data) {
-                data.forEach(student => {
-                    studenti.push(student.dataValues);
-                });
-                grupe.push({grupa:grupa.dataValues,studenti:studenti});
-            });
-
-        });
-        res.json(grupe);
-    });
+    studentiIGrupe(res)
 });
+
+async function studentiIGrupe(res) {
+    res.json(await db.grupa.findAll({include:[{model:db.student, as:"studenti"}]}));
+}
 
 app.get('/v2/dani', function(req, res) {
     db.dan.findAll().then(function(elements) {
@@ -213,7 +205,7 @@ app.get('/v2/tipovi', function(req, res) {
 });
 
 app.get('/v2/tip/:naziv', function(req, res) {
-    db.tip.findOne({where:{naziv:req.params.naziv}}.then(function(data) {
+    db.tip.findOne({where:{naziv:req.params.naziv}}).then(function(data) {
         if(data) {
             res.json(data.dataValues);
         }
@@ -236,13 +228,30 @@ app.get('/v2/tip/:naziv/aktivnosti', function(req, res) {
 });
 
 app.get('/v2/aktivnosti', function(req, res) {
-    db.aktivnost.findAll().then(function(elements) {
+    db.aktivnost.findAll({include:[{model:db.predmet}, {model:db.tip}, {model:db.dan}, {model:db.grupa}]}).then(function(elements) {
         let returnData = [];
 
         elements.forEach(aktivnost => {
-            returnData.push(aktivnost.dataValues);
+            returnData.push({id:aktivnost.dataValues.id, naziv:aktivnost.dataValues.naziv, 
+            pocetak:aktivnost.dataValues.pocetak, kraj:aktivnost.dataValues.kraj, predmet:aktivnost.dataValues.predmet, 
+            grupa:aktivnost.dataValues.grupa, dan:aktivnost.dataValues.dan, tip:aktivnost.dataValues.tip});
         });
         res.json(returnData);
+    });
+});
+
+app.get('/v2/studenti', function(req, res) {
+
+    db.student.findAll({include:[{model:db.grupa, as:"grupe"}]}).then(function(elements) {
+        let studenti = [];
+        elements.forEach(student => {
+            let grupe = [];
+            student.dataValues.grupe.forEach(grupa => {
+                grupe.push({id:grupa.id, naziv:grupa.naziv, predmetId:grupa.predmetId});
+            });
+            studenti.push({id:student.dataValues.id, naziv:student.dataValues.naziv, index:student.dataValues.index, grupe:grupe});
+        });
+        res.json(studenti);
     });
 });
 
@@ -304,6 +313,148 @@ app.post('/v1/aktivnost', function(req, res) {
                 if(err) throw err;
                 res.json({message : "Uspješno dodana aktivnost!"});
             });
+        }
+    });
+});
+
+app.post('/v2/predmet', function(req, res) {
+    let body = req.body;
+
+    db.predmet.findOne({where:{naziv:body.naziv}}).then(function(data) {
+        if(data) {
+            res.json({message:"Predmet već postoji!"});
+            return;
+        }
+        else {
+            db.predmet.create({naziv: body.naziv}).then(function(){res.json({message:"Predmet uspješno kreiran!"})});
+        }
+    });
+});
+
+app.post('/v2/dan', function(req, res) {
+    let body = req.body;
+
+    db.dan.findOne({where:{naziv:body.naziv}}).then(function(data) {
+        if(data) {
+            res.json({message:"Dan već postoji!"});
+            return;
+        }
+        else {
+            db.dan.create({naziv: body.naziv}).then(function(){res.json({message:"Dan uspješno kreiran!"})});
+        }
+    });
+});
+
+app.post('/v2/tip', function(req, res) {
+    let body = req.body;
+
+    db.tip.findOne({where:{naziv:body.naziv}}).then(function(data) {
+        if(data) {
+            res.json({message:"Tip već postoji!"});
+            return;
+        }
+        else {
+            db.tip.create({naziv: body.naziv}).then(function(){res.json({message:"Tip uspješno kreiran!"})});
+        }
+    });
+});
+
+app.post('/v2/grupa', function(req, res) {
+    db.grupa.findOne({where:{naziv:req.body.naziv}}).then(function(data) {
+        if(!data) {
+                db.predmet.findOne({where:{naziv:req.body.predmet.naziv}}).then(function(predmet) {
+                    if(predmet) {
+                        db.grupa.create({naziv:req.body.naziv}).then(function(grupa){
+                            predmet.addGrupeNaPredmetu([grupa]).then(function(element) {
+                                res.json({message:"Grupa uspješno kreirana!"});
+                            });
+                        });
+                    }
+                    else {
+                    res.json({message:"Predmet ne postoji!"});
+                    return;
+                }
+            });
+        }
+        else {
+            res.json({message:"Grupa već postoji!"});
+            return;
+        }
+    });
+});
+
+app.post('/v2/student', function(req, res) {
+
+    db.student.findOne({where:{naziv:req.body.naziv}}).then(function(data) {
+        if(data) {
+            res.json({message:"Student sa tim imenom već postoji!"});
+            return;
+        }
+        else {
+            db.student.findOne({where:{index:req.body.index}}).then(function(element){
+                if(element) {
+                    res.json({message:"Student sa tim indexom već postoji!"});
+                    return;
+                }
+                else {
+                    db.student.create({naziv:req.body.naziv, index:req.body.index}).then(function(output) {
+                        res.json({message:"Student uspješno kreiran!"});
+                        return;
+                    });
+                }
+            });
+        }
+    });
+});
+
+app.post('/v2/aktivnost', function(req, res) {
+
+    db.aktivnost.findAll({where:{[Op.or]:[{naziv:req.body.naziv}, {pocetak:req.body.pocetak}, {kraj:req.body.kraj}]}, include:[{model:db.dan}, {model:db.grupa}]}).then(function(aktivnosti) {
+        if(provjeraValidacijeAktivnosti(aktivnosti, req.body.pocetak, req.body.kraj, req.body.dan.naziv, req.body.grupa.naziv)) {
+            db.predmet.findOne({where:{naziv:req.body.predmet.naziv}}).then(function(predmet) {
+                if(predmet) {
+                    db.grupa.findOne({where:{naziv:req.body.grupa.naziv}}).then(function(grupa) {
+                        if(grupa) {
+                            db.dan.findOne({where:{naziv:req.body.dan.naziv}}).then(function(dan) {
+                                if(dan) {
+                                    db.tip.findOne({where:{naziv:req.body.tip.naziv}}).then(function(tip) {
+                                        if(tip) {
+                                            db.aktivnost.create({naziv:req.body.naziv, pocetak:req.body.pocetak, kraj:req.body.kraj}).then(function(output) {
+                                                predmet.addAktivnostiIzPredmeta([output]);
+                                                grupa.addAktivnostiGrupe([output]);
+                                                dan.addAktivnostiUDanu([output]);
+                                                tip.addAktivnostiTipa([output]);
+                                                res.json({message:"Uspješno kreirana aktivnost!"});
+                                                return;
+                                            });
+                                        }
+                                        else {
+                                            res.json({message:"Tip nije validan!"});
+                                            return;
+                                        }
+                                    });
+                                }
+                                else {
+                                    res.json({message:"Dan nije validan!"});
+                                    return;
+                                }
+                            });
+                        }
+                        else {
+                            res.json({message:"Grupa nije validna!"});
+                            return;
+                        }
+                    });
+                }
+                else {
+                    res.json({message:"Predmet nije validan!"});
+                    return;
+                }
+            });
+        }
+        else {
+            res.json({message:"Aktivnost već postoji ili nije unesena validna aktivnost!"});
+            return;
         }
     });
 });
@@ -394,6 +545,213 @@ app.delete('/v1/all', function(req, res) {
 
     res.json({message: "Uspješno obrisan sadržaj datoteka!"});
 });
+
+app.delete('/v2/predmet/:id', function(req, res) {
+    db.predmet.findOne({where:{id:req.params.id}}).then(function(predmet) {
+        if(predmet) {
+            predmet.destroy();
+            res.json({message:"Uspješno obrisan predmet!"});
+            return;
+        }
+        res.json({message:"Predmet nije validan!"});
+    });
+});
+
+app.delete('/v2/dan/:id', function(req, res) {
+    db.dan.findOne({where:{id:req.params.id}}).then(function(dan) {
+        if(dan) {
+            dan.destroy();
+            res.json({message:"Uspješno obrisan dan!"});
+            return;
+        }
+        res.json({message:"Dan nije validan!"});
+    });
+});
+
+app.delete('/v2/tip/:id', function(req, res) {
+    db.tip.findOne({where:{id:req.params.id}}).then(function(tip) {
+        if(tip) {
+            tip.destroy();
+            res.json({message:"Uspješno obrisan tip!"});
+            return;
+        }
+        res.json({message:"Tip nije validan!"});
+    });
+});
+
+app.delete('/v2/student/:id', function(req, res) {
+    db.student.findOne({where:{id:req.params.id}}).then(function(student) {
+        if(student) {
+            student.destroy();
+            res.json({message:"Uspješno obrisan student!"});
+            return;
+        }
+        res.json({message:"Student nije validan!"});
+    });
+});
+
+app.delete('/v2/grupa/:id', function(req, res) {
+    db.grupa.findOne({where:{id:req.params.id}}).then(function(grupa) {
+        if(grupa) {
+            grupa.destroy();
+            res.json({message:"Uspješno obrisana grupa!"});
+            return;
+        }
+        res.json({message:"Grupa nije validan!"});
+    });
+});
+
+app.delete('/v2/aktivnost/:id', function(req, res) {
+    db.aktivnost.findOne({where:{id:req.params.id}}).then(function(aktivnost) {
+        if(aktivnost) {
+            aktivnost.destroy();
+            res.json({message:"Uspješno obrisana aktivnost!"});
+            return;
+        }
+        res.json({message:"Predmet nije validan!"});
+    });
+});
+
+//**
+    //PUT
+//**
+app.put('/v2/student/:id', function(req, res) {
+    db.student.findOne({where:{index:req.body.index}}).then(function(student) {
+        if(student) {
+            if(student.dataValues.id == req.params.id) {
+                db.student.update(req.body, {where:{id:req.params.id}}).then(function(output) {
+                    if(output[0]) {
+                        res.json({message:"Uspješno ažuriran student!"});
+                        return;
+                    }
+                    res.json({message:"Student nije validan!"});
+                });
+            }
+            else {
+                res.json({message:"Student sa tim indeksom već postoji!"});
+            }
+        }
+        else {
+            db.student.update(req.body, {where:{id:req.params.id}}).then(function(output) {
+                if(output[0]) {
+                    console.log(output);
+                    res.json({message:"Uspješno ažuriran student!"});
+                    return;
+                }
+                res.json({message:"Student nije validan!"});
+            });
+        }
+    });
+});
+
+app.put('/v2/predmet/:id', function(req, res) {
+    db.predmet.findOne({where:{naziv:req.body.naziv}}).then(function(predmet) {
+        if(predmet) {
+            if(predmet.dataValues.id == req.params.id) {
+                db.predmet.update(req.body, {where:{id:req.params.id}}).then(function(output) {
+                    if(output[0]) {
+                        res.json({message:"Uspješno ažuriran predmet!"});
+                        return;
+                    }
+                    res.json({message:"Predmet nije validan!"});
+                });
+            }
+            else {
+                res.json({message:"Predmet sa tim nazivom već postoji!"});
+                return;
+            }
+        }
+        else {
+            db.predmet.update(req.body, {where:{id:req.params.id}}).then(function(output) {
+                if(output[0]) {
+                    res.json({message:"Uspješno ažuriran predmet!"});
+                    return;
+                }
+                res.json({message:"Predmet nije validan!"});
+            });
+        }
+    });
+});
+
+app.put('/v2/grupa/:id', function(req, res) {
+    db.grupa.findOne({where:{naziv:req.body.naziv}}).then(function(grupa) {
+        if(grupa) {
+            if(grupa.dataValues.id == req.params.id) {
+                    db.grupa.update(req.body, {where:{id:req.params.id}}).then(function(azuriranaGrupa) {
+                        if(azuriranaGrupa[0]) {
+                            res.json({message:"Uspješno ažurirana grupa!"});
+                            return;
+                        }
+                        else {
+                            res.json({message:"Grupa nije validna!"});
+                        }
+                    }).catch(function(err) {res.json({message:"Unesen je nevalidan id za predmet i/ili grupu!"})});
+            }
+            else {
+                res.json({message:"Postoji grupa sa nazivom " + req.body.naziv + ", a sa drugim id-em!"});
+                return;
+            }
+        }
+        else {
+            db.grupa.update(req.body, {where:{id:req.params.id}}).then(function(azuriranaGrupa) {
+                if(azuriranaGrupa[0]) {
+                    res.json({message:"Uspješno ažurirana grupa!"});
+                    return;
+                }
+                else {
+                    res.json({message:"Grupa nije validna!"});
+                }
+            }).catch(function(err) {res.json({message:"Unesen je nevalidan id za predmet i/ili grupu!"})});
+        }
+    });
+});
+
+app.put('/v2/tip/:id', function(req, res) {
+    db.tip.update(req.body, {where:{id:req.params.id}}).then(function(output) {
+        if(output[0]) {
+            res.json({message:"Uspješno ažuriran tip!"});
+        }
+        else {
+            res.json({message:"Tip nije validan!"});
+        }
+    });
+});
+
+app.put('/v2/dan/:id', function(req, res) {
+    db.dan.update(req.body, {where:{id:req.params.id}}).then(function(output) {
+        if(output[0]) {
+            res.json({message:"Uspješnp ažuriran dan!"});
+        }
+        else {
+            res.json({message:"Dan nije validan!"});
+        }
+    });
+});
+
+app.put('/v2/aktivnost/:id',function(req, res) {
+    db.aktivnost.findAll({where:{danId:req.body.danId}}).then(function(aktivnosti) {
+        aktivnosti.forEach(aktivnost => {
+            if(aktivnost.pocetak <= req.body.pocekta && req.body.pocetak < aktivnost.kraj ||
+                req.body.kraj > aktivnost.pocetak && req.body.kraj <= aktivnost.kraj ||
+                req.body.pocetak <= aktivnost.pocetak && aktivnost.pocetak < req.body.kraj ||
+                aktivnost.pocetak == req.body.pocetak  && aktivnost.kraj == req.body.kraj) {
+                    res.json({message:"Aktivnost nema validno početno ili krajnje vrijeme!"});
+                    return;    
+                }
+            
+        });
+
+        db.aktivnost.update(req.body, {where:{id:req.body.id}}).then(function(output) {
+            if(output[0]) {
+                res.json({message:"Uspješno ažurirana aktivnost!"});
+            }
+            else {
+                res.json({message:"Aktivnost nije validna!"});
+            }
+        }).catch(function(err){res.json({message:"Nepravilan id aktivnosti!"})});
+    }).catch(function(err){res.json({message:"Nepravilno unesen id (ili više njih) u aktivnosti!"})});
+});
+
 app.listen(3000);
 
 //Provjerava da li se nalazi predmet u predmetima.txt
@@ -454,4 +812,26 @@ function toJSONPredmet(text = "") {
         }
     }
     return (array);
+}
+
+function provjeraValidacijeAktivnosti(aktivnosti, pocetakAktivnosti, krajAktivnosti, nazivDana, nazivGrupe) {
+
+    if(!(pocetakAktivnosti > 0  && pocetakAktivnosti < 24) || !(krajAktivnosti > 0  && krajAktivnosti < 24) ||
+         pocetakAktivnosti >= krajAktivnosti || !Number.isInteger(pocetakAktivnosti*2) || !Number.isInteger(krajAktivnosti*2)) {
+        return false;
+    }
+    var ok = true;
+    aktivnosti.forEach(aktivnost => {
+        if(aktivnost.dataValues.dan.dataValues.naziv == nazivDana) {
+            if(aktivnost.dataValues.grupa.dataValues.naziv == nazivGrupe) {
+                if(aktivnost.pocetak <= pocetakAktivnosti && pocetakAktivnosti < aktivnost.kraj ||
+                    krajAktivnosti > aktivnost.pocetak && krajAktivnosti <= aktivnost.kraj ||
+                    pocetakAktivnosti <= aktivnost.pocetak && aktivnost.pocetak < krajAktivnosti ||
+                    aktivnost.pocetak == pocetakAktivnosti  && aktivnost.kraj == krajAktivnosti) {
+                        ok = false;
+                    }
+            }
+        }
+    });
+    return ok;
 }
